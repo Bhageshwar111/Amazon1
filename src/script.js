959a488d7812c77d624a4dcd6d0d2910c057aee0
Bhageshwar <bhageshwar8d@gmail.com>
2025-05-10 11:24:17 +0530

    gjdsvbhjsdk

--- src/script.js
+++ src/script.js
@@ -1,39 +1,38 @@
-let a=``
 
             let data=[
-                {title:"Continue shopping deals",img1:"image/Phone1.jpg",img2:"",img3:"",img4:"",text1:"fsd",text2:"cvxf",text3:"",text4:""},
-                {title:"",img1:"image/Phone2.jpg",img2:"",img3:"",img4:"",text1:"fsd",text2:"",text3:"",text4:""},
-                {title:"",img1:"image/Phone3.jpg",img2:"",img3:"",img4:"",text1:"chgf",text2:"",text3:"",text4:""}
+                {title:"Continue shopping deals",img1:"image/Main photo.jpg",img2:"image/Phone1.jpg",img3:"image/Phone2.jpg",img4:"image/Phone3.jpg",text1:"Phone 1",text2:"Phone 2",text3:"Phone 3",text4:"Phone 4",link:"See all deals"},
+                {title:"Appliances for your home | Up to 55% off",img1:"",img2:"",img3:"",img4:"",text1:"Air conditioners",text2:"Refrigerators",text3:"Microwaves",text4:"Washing macchines",link:"See more"},
+                {title:"Revamp your home in style",img1:"",img2:"",img3:"",img4:"",text1:"Cushion covers, bedsheets & more",text2:"Figurines, vases & more",text3:"Home storage",text4:"Lighting solutions",link:"Explore all"}
             ]
 
             let bag="";
 
 
             data.map((k)=>{
-                return bag+=`<div class="h-[400px] w-[400px] bg-white flex flex-col justify-between p-2">
+                return bag+=`<div class="h-[490px] w-[400px] bg-gray-100 flex flex-col justify-between p-2">
 
                 <div class="flex flex-col items-center justify-between">
-                    <div class="font-bold text-4xl">${k.title}</div>
-                    <div class="flex justify-around items-center gap-10">
+                    <div class="font-bold text-2xl mb-3">${k.title}</div>
+                    <div class="flex justify-around items-center gap-10 mb-1">
                         <img class="h-40 w-40" src="${k.img1}" alt="">
                         <img class="h-40 w-40" src="${k.img2}" alt="">
                     </div>
-                    <div class="flex justify-around w-[400px] text-xs">
+                    <div class="flex justify-around w-[400px] text-xs mb-1">
                         <div>${k.text1}</div>
                         <div>${k.text2}</div>
                     </div>
-                    <div class="flex justify-around items-center gap-10">
+                    <div class="flex justify-around items-center gap-10 mb-1">
                         <img class="h-40 w-40" src="${k.img3}" alt="">
                         <img class="h-40 w-40" src="${k.img4}" alt="">
                     </div>
-                    <div class="flex justify-around w-[400px] text-xs">
+                    <div class="flex justify-around w-[400px] text-xs mb-1">
                         <div>${k.text3}</div>
                         <div>${k.text4}</div>
                     </div>
                 </div>
 
                 <div class="text-xs text-blue-600 font-semibold">
-                    <a href="#">See all deals</a>
+                    <a href="#">${k.link}</a>
                 </div>
 
 
